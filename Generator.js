31b0d79577a5ac69dd56a8e08190ef82d1f741c7
Pixel8882 <153401920+Pixel8882@users.noreply.github.com>
2023-12-10 01:20:26 +0100

    Add files via upload

---
+++ Generator.js
@@ -0,0 +1,12 @@
+elements.Steam-turbine = {
+    behavior: behaviors.WALL,
+    reactions: {
+        "steam": { "charge1": 1 }
+    },
+    color: "#8A0808",
+    colorOn: "##5858FA",
+    category: "machines",
+    tempHigh: 1500,
+    stateHigh: "molten_iron",
+    conduct: 1
+};
